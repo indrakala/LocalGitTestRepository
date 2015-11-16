6cbc84d25c12549ebb5d8e730a435adb0dab3257
lavanya <lavanya@Lenovo-PC>
2015-11-16 15:21:32 +0530

    shankari

---
+++ GitTest/src/Testing/Test.java
@@ -0,0 +1,18 @@
+package Testing;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20;
+		int sum=a+b;
+		System.out.println(sum);
+		
+
+	}
+
+}
